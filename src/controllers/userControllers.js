5b5aef378ad45e40874de6452bcd4407e9bfaeca
Yang Lu (Leon) <yanglyu.leon.7@gmail.com>
2019-03-26 16:26:30 -0700

    demo: Adding the handlers for login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -21,9 +21,26 @@ export const register = (req, res) => {
 }
 
 export const login = (req, res) => {
-
+    User.findOne({
+        email: req.body.email,
+    }, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return res.status(401).json({ message: 'Authentication failed. No user found!' });
+        } else if (user) {
+            if (!user.comparePassword(req.body.password, user.hashPassword)) {
+                return res.status(401).json({ message: 'Authentication failed. Wrong password!' });
+            }
+        } else {
+            return res.json({ token: jwt.sign({ email: user.email, username: user.username, _id: user._id }, 'RESTFULAPIs') });
+        }
+    });
 }
 
-export const loginRequired = (req, res) => {
-
+export const loginRequired = (req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        return res.status(401).json({ message: 'Unauthorized user!' });
+    }
 }
